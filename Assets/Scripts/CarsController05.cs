a993389404b71df55b80b7a439fd2066b8723395
Shinoda Naoki <shinoda.naoki@techcross.co.jp>
2019-06-30 13:11:18 +0900

    new scene `road06` duplicated from `road05`.

---
+++ Assets/Scripts/CarsController05.cs
@@ -0,0 +1,234 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using Random = UnityEngine.Random;
+
+using Car = ICar<Car03s, Car03d>;
+using CarRepository = CarRepository<Car03s, Car03d>;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// 沢山の車を管理するクラス
+/// </summary>
+public partial class CarsController05 : MonoBehaviour
+{
+#if CPU_DRIVING
+    const int MAX_CARS = 100;
+#else
+    const int MAX_CARS = 400;
+#endif
+
+    /// <summary>
+    /// 車をレンダリングするシェーダー
+    /// </summary>
+    public Shader carShader;
+
+    /// <summary>
+    /// 車の移動を行うコンピュートシェーダー
+    /// </summary>
+    public ComputeShader carComputeShader;
+
+    /// <summary>
+    /// 情報表示用
+    /// </summary>
+    public Text statusText;
+
+    /// <summary>
+    /// 車のマテリアル
+    /// </summary>
+    Material material;
+
+    /// <summary>
+    /// 車ファクトリ
+    /// </summary>
+    CarRepository factory;
+
+    /// <summary>
+    /// 路面オブジェクト
+    /// </summary>
+    RoadPlane02 roadPlane;
+
+    /// <summary>
+    /// 破棄
+    /// </summary>
+    void OnDisable()
+    {
+        // コンピュートバッファは明示的に破棄しないと怒られます
+        factory.ReleaseBuffers();
+    }
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    void Start()
+    {
+        material = new Material(carShader);
+        roadPlane = GetComponent<RoadPlane02>();
+        InitializeComputeBuffer();
+    }
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    void Update()
+    {
+#if CPU_DRIVING
+#else
+        carComputeShader.SetBuffer(0, "CarsStatic", factory.StaticInfoBuffer);
+        carComputeShader.SetBuffer(0, "CarsDynamic", factory.DynamicInfoBuffer);
+        carComputeShader.SetInt("count", factory.ActiveCars);
+        carComputeShader.SetFloat("DeltaTime", Time.deltaTime);
+        var carnum = factory.ActiveCars;
+        carComputeShader.Dispatch(0, carnum / 8 + 1, 1, 1);
+#endif
+    }
+
+    /// <summary>
+    /// コンピュートバッファの初期化
+    /// </summary>
+    void InitializeComputeBuffer()
+    {
+        factory = new CarRepository(MAX_CARS, CarTemplate03.dictionary);
+        factory.AssignBuffers();
+
+
+        StartCoroutine(WatchLoop(OnEachScan, OnEachElement));
+
+        factory.ApplyData();
+    }
+
+    const int MIN_INTERVAL_FRAMES = 100;
+    private Dictionary<int, int> _lastEntries = new Dictionary<int, int>();
+
+    private void OnEachScan(Car[] cars)
+    {
+        if (factory.ActiveCars >= MAX_CARS) return;
+
+        var entries = roadPlane.EntryPoints;
+
+        // 車を追加する
+        int index = Random.Range(0, entries.Count);
+        int f;
+        if( _lastEntries.TryGetValue(index, out f) && Time.frameCount - f < MIN_INTERVAL_FRAMES)
+        { // 最初から衝突するのを避けるため、一定フレーム数内に車が入ったばかりなら次の機会を待つ
+            return; 
+        }
+
+        var entry = entries[index];
+        factory.CreateRandomType(entry.pos, entry.dir);
+        _lastEntries[index] = Time.frameCount;
+        factory.ApplyData();
+    }
+
+    private void OnEachElement(int index, Car car)
+    {
+        if ( roadPlane.isInside(car.Dynamic.pos) ) return;
+
+        factory.Remove(index);
+        factory.ApplyData();
+    }
+
+#if CPU_DRIVING
+    private IEnumerator WatchLoop(Action<Car[]> onScan, Action<int, Car> onElement)
+    {
+        while (true)
+        {
+            Car[] carsArray = factory.GetCars();
+
+            //FullDump(carsArray);
+
+            onScan(carsArray);
+            int cars = factory.ActiveCars;
+            for (int index = 0; index < cars; index++)
+            {
+                Drive(carsArray, index);
+                onElement(index, carsArray[index]);
+            }
+            factory.ApplyData();
+            yield return null;
+        }
+    }
+
+    private void FullDump(Car[] carsArray)
+    {
+        var sb = new StringBuilder();
+        for (int i = 0; i < MAX_CARS; i++)
+        {
+            var car = carsArray[i];
+            if (car.Static.size.z == 0)
+            {
+                sb.AppendFormat("#{0} Empty", i);
+                sb.AppendLine();
+                continue;
+            }
+            sb.Append(DebugStr(i, car));
+            sb.AppendLine();
+        }
+        Debug.Log(sb.ToString());
+    }
+#else
+    private int _currentIndex = 0;
+    private IEnumerator WatchLoop(Action<Car[]> onScan, Action<int, Car> onElement)
+    {
+        // １フレーム中に進むカウント数
+        const int COUNT_PER_FRAME = 10;
+        Car[] carsArray = factory.GetCars();
+        onScan(carsArray);
+        while (true) {
+
+            int cars = factory.ActiveCars;
+            if (cars > 0)
+            {
+                if (_currentIndex >= cars)
+                {
+                    _currentIndex = 0;
+                    carsArray = factory.GetCars();
+                    onScan(carsArray);
+                    yield return null;
+                    continue;
+                }
+                var car = carsArray[_currentIndex];
+                onElement(_currentIndex, car);
+                _currentIndex++;
+
+                // 
+                if (_currentIndex % COUNT_PER_FRAME == 0)
+                {
+                    carsArray = factory.GetCars();
+                    onScan(carsArray);
+                    yield return null;
+                    continue;
+                }
+            }
+        }
+    }
+
+#endif
+
+
+    /// <summary>
+    /// レンダリング
+    /// </summary>
+    void OnRenderObject()
+    {
+        // 車データバッファをマテリアルに設定
+        material.SetBuffer("CarsStatic", factory.StaticInfoBuffer);
+        material.SetBuffer("CarsDynamic", factory.DynamicInfoBuffer);
+        // レンダリングを開始
+        material.SetPass(0);
+        // オブジェクトをレンダリング
+        Graphics.DrawProcedural(MeshTopology.Points, factory.ActiveCars);
+    }
+
+    /// <summary>
+    /// StatusTextへの表示用
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return factory.ActiveCars.ToString() + " Cars";
+    }
+}
---
+++ Assets/Scripts/CarsController05.driving.cs
@@ -0,0 +1,204 @@
+using UnityEngine;
+using Car = ICar<Car03s, Car03d>;
+using Car_S = Car03s;
+using Car_D = Car03d;
+
+#if CPU_DRIVING
+/// <summary>
+/// 沢山の車を管理するクラス
+/// </summary>
+public partial class CarsController05 : MonoBehaviour
+{
+    private const float MAX_FORCAST_COUNT = 20f;
+
+    // 2Dベクトル外積
+    private float cross2d(Vector2 a, Vector2 b)
+    {
+        return a.x * b.y - a.y * b.x;
+    }
+
+    // 2Dベクトル内積
+    private float dot2d(Vector2 a, Vector2 b)
+    {
+        return a.x * b.x + a.y * b.y;
+    }
+
+    // 2Dベクトル回転
+    private Vector2 rotate2d(Vector2 v2d, Quaternion mat)
+    {
+        return mat * v2d;
+    }
+
+    // 指定方向に最も張り出した頂点の距離から、最大衝突距離を計算する
+    private void GetSafeDistance(Car_S carS, Car_D carD, Vector2 diffNml, out float distance)
+    {
+        // Y軸回転の行列を作る
+        Quaternion _matrix = Quaternion.FromToRotation(carD.direction, diffNml);
+
+        // 車の四方の頂点を評価して、最大値を保存する
+        Vector2 vert = new Vector2(carS.size.x, carS.size.z) * 0.5f; // (x,y)
+                                                                     // dir は (1,0) が進行方向になっているので、回転後のy座標が diffPosNml との内積に等しい
+        distance = rotate2d(vert, _matrix).y;
+        vert *= -1; // (-x,-y)
+        distance = Mathf.Max(distance, rotate2d(vert, _matrix).y);
+        vert.x *= -1; // (x,-y)
+        distance = Mathf.Max(distance, rotate2d(vert, _matrix).y);
+        vert *= -1; // (-x,y)
+        distance = Mathf.Max(distance, rotate2d(vert, _matrix).y);
+    }
+
+    // 最も衝突の可能性の高い車のid(index)を返す
+    private void FindMostDangerCar(Car[] cars, int id1, int id2, ref float timeMin, ref int idMin)
+    {
+        if (timeMin <= 0) return; // 時すでに遅し
+
+        var carD1 = cars[id1].Dynamic;
+        var carD2 = cars[id2].Dynamic;
+        // 相対位置ベクトル
+        Vector2 diffPos = carD2.pos - carD1.pos;
+        // 相対速度ベクトル
+        Vector2 diffVel = carD1.direction * carD1.velocity - carD2.direction * carD2.velocity;
+
+        float dotPosAndVel = dot2d(diffPos, diffVel);
+        // 内積が０以下なら衝突の可能性なし！
+        if (dotPosAndVel < 0)
+        {
+            return;
+        }
+
+        // 背後から接近してくるものは回避しない（相手任せ）
+        if (dot2d(diffPos, carD1.direction) < 0)
+        {
+            return;
+        }
+
+        float absVel = diffVel.magnitude;
+        if (absVel < 0.001f) return; // 接近していない
+
+        float countAssume = dotPosAndVel / absVel;
+        if (countAssume > MAX_FORCAST_COUNT)
+        {
+            return; // 遠い未来過ぎるので無視
+        }
+
+        // 二つの車のサイズを考慮した距離を求める
+        var carS1 = cars[id1].Static;
+        var carS2 = cars[id2].Static;
+        Vector2 diffVelNml = diffVel.normalized;
+        Vector2 diffPosNml = diffPos.normalized;
+        float d1, d2;
+        GetSafeDistance(carS1, carD1, diffVelNml, out d1);
+        GetSafeDistance(carS2, carD2, -diffVelNml, out d2);
+        float distance = d1 + d2;
+
+        // 最接近点での距離が二つの車のサイズを考慮した距離以上なら衝突しない
+        float crossPosAndVel = Mathf.Abs(cross2d(diffPos, diffVelNml));
+        if (crossPosAndVel > distance)
+        {
+            return;
+        }
+
+        // どちらかが高速で移動しているなら停止距離には余裕を持つ
+        //distance += Mathf.Max(carD2.velocity, carD1.velocity) * 0.28f;
+
+        float t = Mathf.Max(0, (dotPosAndVel - distance) / absVel);
+        // このままだと近い将来衝突しそう
+        if (t > timeMin)
+        {
+            return; // もっと近い相手が既にいる
+        }        
+
+        // 最小値更新
+        timeMin = t;
+        idMin = id2;
+        if(carD1.direction.y >= 0)
+        Debug.Log(
+            string.Format("<color='grey'>{0}<->{1}:\n</color> t0={2:0.0}, v={3:0.0}, dist={4:0.0}+{5:0.0}, t={6:0.0}({7:0.0})",
+                DebugStr(id1, cars[id1]), DebugStr(id2, cars[id2]), countAssume, absVel, d1, d2, t, timeMin));
+
+    }
+
+    private void DriveAll()
+    {
+        var cars = factory.GetCars();
+        int c = factory.ActiveCars;
+        for (int i = 0; i < c; i++)
+        {
+            Drive(cars, i);
+        }
+        factory.ApplyData();
+    }
+
+
+    private void Drive(Car[] cars, int id)
+    {
+        if (cars[id].Static.size.z == 0) return; // 削除済みのデータ
+
+        int count = factory.ActiveCars;
+        int idMin = id;
+        float timeMin = 1000; // このままだとあと何回で衝突するか
+        for (int i = 0; i < count; i++)
+        {
+            if (i == id) continue;
+            if (cars[i].Static.size.z == 0) break; // 削除済みのデータ＝末端に到達
+            FindMostDangerCar(cars, id, i, ref timeMin, ref idMin);
+        }
+
+        var carD = cars[id].Dynamic;
+        var carS = cars[id].Static;
+
+        if (idMin == id)
+        { // 衝突の可能性の高い車はない
+            if (carD.velocity < carS.idealVelocity)
+            {
+                var p = carD.velocity;
+                carD.velocity = Mathf.Min(carD.velocity + carS.mobility * 1f, carS.idealVelocity);
+
+                if (carD.direction.y >= 0)
+                    Debug.Log(string.Format("{0}:\n <color='blue'>vel={1:0.0}</color>",
+                    DebugStr(id, cars[id]), carD.velocity - p));
+
+            }
+        }
+        else
+        {
+            if (carD.velocity > 0)
+            {
+                var p = carD.velocity;
+                carD.velocity = Mathf.Max(0, carD.velocity - carS.mobility * 50f / Mathf.Max(0.1f, timeMin));
+
+                if (carD.direction.y >= 0)
+                {
+                    if ((cars[idMin].Dynamic.pos - carD.pos).magnitude < 5)
+                    {
+                        carD.velocity = 0;
+                        Debug.LogErrorFormat(string.Format("{0}<->{1}:\n <color='magenta'>!!CRUSH!!</color>", 
+                            DebugStr(id, cars[id]), DebugStr(idMin, cars[idMin])));
+                    }
+                    else
+                    {
+                        Debug.Log(string.Format("{0}<->{1}:\n <color='red'>t={2:0.0}, v={3:0.0})</color>",
+                            DebugStr(id, cars[id]), DebugStr(idMin, cars[idMin]), timeMin, carD.velocity - p));
+                    }
+                }
+            }
+        }
+
+        // それぞれの位置情報に移動ベクトルを加算　(0.28はkm/hをm/sに変換する係数)
+        carD.pos += carD.direction * carD.velocity * Time.deltaTime * 0.28f;
+        cars[id].Dynamic = carD;
+
+    }
+
+    private string DebugStr(int id, Car car)
+    {
+        var v = car.Dynamic.direction * car.Dynamic.velocity;
+        return string.Format(
+            "<color='#{1}'>■</color>{0}({2:0.0},{3:0.0})⇒({4:0.0},{5:0.0})",
+                    id, ColorUtility.ToHtmlStringRGB(car.Static.color),
+                    car.Dynamic.pos.x, car.Dynamic.pos.y,
+                    v.x, v.y
+        );
+    }
+}
+#endif
